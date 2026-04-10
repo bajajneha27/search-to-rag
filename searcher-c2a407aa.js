36643b0127fcd5f6acf1406b5e7921aecd38cc7d
bajajneha27 <bajajneha27@users.noreply.github.com>
2026-04-10 00:47:37 +0000

    deploy: 158be7f1cd151db738ee92b2436265a509deac5f

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-3f1af10d.js',
+                path_to_root + 'searchindex-7dd0a5f2.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
